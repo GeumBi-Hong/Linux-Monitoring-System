96f9d97e75c03ae8547b181c564239905b48b6cf
ghdrmaql11 <58154655+ghdrmaql11@users.noreply.github.com>
2021-11-02 17:51:24 +0900

    Delete Web directory

--- Web/template/session.js
+++
@@ -1,5 +0,0 @@
-function sess (obj) {
-    sessionStorage.setItem("cur_single_IP",obj.getAttribute('ip_number'));
-console.log(sessionStorage.getItem("cur_single_IP"));
-    document.location.reload(true);
-}
